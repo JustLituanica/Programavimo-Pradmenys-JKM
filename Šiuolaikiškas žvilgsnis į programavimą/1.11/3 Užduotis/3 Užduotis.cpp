5e1f9c04166af52a6a0f3e7f1c4d661ed86d8b4d
JustLituanica <justinas.launikonis@gmail.com>
2023-04-15 22:07:15 +0300

    1.11 Skyrius

---
+++ Šiuolaikiškas žvilgsnis į programavimą/1.11/3 Užduotis/3 Užduotis.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const char CDfv[] = "Duomenys.txt";
+const char CRfv[] = "Rezultatai.txt";
+
+int Kiek();
+void Spausdinti(int ats);
+
+int main()
+{
+    int ats;
+
+    ats = Kiek();
+
+    Spausdinti(ats);
+
+    return 0;
+}
+
+int Kiek()
+{
+    int k = 0;
+    char s1, s2;
+
+    s1 = ' ';
+
+    ifstream fd(CDfv);
+
+    while (!fd.eof())
+    {
+        fd.get(s2);
+        if (!fd.eof() && (s1 == '/') && (s2 == '/'))
+        {
+            k++;
+        }
+        s1 = s2;
+    }
+
+    return k;
+}
+
+void Spausdinti(int ats)
+{
+    ofstream fr(CRfv);
+
+    fr << ats;
+
+    fr.close();
+}
---
+++ Šiuolaikiškas žvilgsnis į programavimą/1.11/4 Užduotis/4 Užduotis.cpp
@@ -0,0 +1,70 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const char CDfv[] = "Duomenys.txt";
+const char CRfv[] = "Rezultatai.txt";
+
+int Kiek(char sim);
+void Spausdinti(int raides, int skaiciai, int tarpai, int simboliai);
+
+int main()
+{
+    int skaiciai = 0, raides = 0;
+    int tarpai, simboliai = 0;
+
+    for (char sim = 'a'; sim <= 'z'; sim++)
+    {
+        raides = raides + Kiek(sim);
+    }
+
+    for (char sim = '1'; sim <= '9'; sim++)
+    {
+        skaiciai = skaiciai + Kiek(sim);
+    }
+
+    tarpai = Kiek(' ');
+
+    simboliai = simboliai + Kiek('.');
+    simboliai = simboliai + Kiek(',');
+    simboliai = simboliai + Kiek('-');
+    simboliai = simboliai + Kiek('(');
+    simboliai = simboliai + Kiek(')');
+
+    Spausdinti(raides, skaiciai, tarpai, simboliai);
+
+    return 0;
+}
+
+int Kiek(char sim)
+{
+    char s;
+    int k = 0;
+
+    ifstream fd(CDfv);
+
+    while (!fd.eof())
+    {
+        fd.get(s);
+        if (!fd.eof() && s == sim)
+        {
+            k++;
+        }
+    }
+
+    return k;
+}
+
+void Spausdinti(int raides, int skaiciai, int tarpai, int simboliai)
+{
+    ofstream fr(CRfv);
+
+    fr << "Raidziu yra: " << raides << endl;
+    fr << "Skaitmenu yra: " << skaiciai << endl;
+    fr << "Tarpo simboliu yra: " << tarpai << endl;
+    fr << "Kitokiu simboliu yra: " << simboliai;
+
+    fr.close();
+}
