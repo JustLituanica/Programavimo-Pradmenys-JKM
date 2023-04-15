576b553cde1799b23c96107e54b3ec9c8e6e96d1
JustLituanica <justinas.launikonis@gmail.com>
2023-04-15 20:15:10 +0300

    1.11 Skyrius

---
+++ Šiuolaikiškas žvilgsnis į programavimą/1.11/2 Užduotis/2 Užduotis.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const char CDfv[] = "Duomenys.txt";
+const char CRfv[] = "Rezultatai.txt";
+const int CMax = 100;
+
+int Kiek();
+void Spausdinti(int ats);
+
+int main()
+{
+    int ats;
+
+    ats = Kiek();
+    Spausdinti(ats);
+
+    return 0;
+}
+
+int Kiek()
+{
+    char s1, s2;
+    int k = 0;
+
+    ifstream fd(CDfv);
+
+    s1 = ' ';
+
+    while (!fd.eof())
+    {
+        fd.get(s2);
+        if (!fd.eof() && s1 == 'a' && s2 == 'u')
+        {
+            k++;
+        }
+        s1 = s2;
+    }
+    return k;
+}
+
+void Spausdinti(int ats)
+{
+    ofstream fr(CRfv);
+
+    fr << ats;
+
+    fr.close();
+}
