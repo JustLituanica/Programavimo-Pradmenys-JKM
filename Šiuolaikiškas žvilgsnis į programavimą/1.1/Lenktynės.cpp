e0c5ad28b85a47235bc76fe0419df51f8b6779f9
JustLituanica <justinas.launikonis@gmail.com>
2023-04-16 12:58:48 +0300

    1.1 Skyrius

---
+++ Šiuolaikiškas žvilgsnis į programavimą/1.1/Lenktynės.cpp
@@ -0,0 +1,69 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int kt, kv, km, gt, gv, gm;
+    int i = 0;
+
+    cout << "Tigras atsiliko: ";
+    cin >> kt;
+
+    cout << "Vilkas atsiliko: ";
+    cin >> kv;
+
+    cout << "Mokytojas atsiliko: ";
+    cin >> km;
+
+    cout << "Tigro greitis: ";
+    cin >> gt;
+
+    cout << "Vilko greitis: ";
+    cin >> gv;
+
+    cout << "Mokytojo greitis: ";
+    cin >> gm;
+
+    while ((i <= 8) && (kt >= 0) && (kv >= 0) && (km >= 0))
+    {
+        kt = kt - gt + 1;
+        kv = kv - gv + 1;
+        km = km - gm + 1;
+        i++;
+    }
+
+    if (kt <= 0)
+    {
+        cout << "Tigras pavijo" << endl;
+    }
+    else
+    {
+        cout << "Tigras nepavijo" << endl;
+    }
+
+    if (kv <= 0)
+    {
+        cout << "Vilkas pavijo" << endl;
+    }
+    else
+    {
+        cout << "Vilkas nepavijo" << endl;
+    }
+
+    if (km <= 0)
+    {
+        cout << "Mokytojas pavijo" << endl;
+    }
+    else
+    {
+        cout << "Mokytojas nepavijo" << endl;
+    }
+
+    if (kt > 0 && kv > 0 && km > 0)
+    {
+        cout << "Niekas nepavijo";
+    }
+
+    return 0;
+}
